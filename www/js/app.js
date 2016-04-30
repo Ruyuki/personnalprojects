179c6ea5204c3fb970b8c46bb82bb014a000309e
Florent <Florent>
2016-04-30 16:19:15 +0200

    Correction pour faire fonctionner l'appli angular + ajout de l'icone menu

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
-angular.module('toolbarDemo1', ['ngMaterial'])
+angular.module('chezCecEtFlo', ['ngMaterial'])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('teal')
+            .accentPalette('light-green')
+            .warnPalette('amber');
+    })
     .controller('AppCtrl', function($scope) {
         $scope.title = 'test title';
     });
