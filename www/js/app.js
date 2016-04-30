83662b4e1104811d1e62481782afca578a260efa
Florent <Florent>
2016-04-30 18:42:02 +0200

    Ajout d'icone et mise en page du menu

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,39 @@ angular.module('chezCecEtFlo', ['ngMaterial'])
         $mdThemingProvider.theme('default')
             .primaryPalette('teal')
             .accentPalette('light-green')
-            .warnPalette('amber');
+            .warnPalette('amber')
+            .backgroundPalette('grey');
     })
-    .controller('AppCtrl', function($scope) {
-        $scope.title = 'test title';
-    });
+    .controller('AppCtrl', function($scope, $mdSidenav, $log, $timeout) {
+        $scope.toggleLeft = buildDelayedToggler('left');
+
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+            }, 200);
+        }
+        
+        function debounce(func, wait, context) {
+            var timer;
+            return function debounced() {
+                var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+    }).controller('LeftCtrl', function ($scope, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('left').close()
+            .then(function () {
+                $log.debug("close LEFT is done");
+            });
+        };
+    })
