40fe845ab07a0c9588816f6a610ec4102707067c
Florent <Florent>
2016-05-01 10:12:19 +0200

    Mise en place du routage angular + finalisation du menu

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,11 +1,32 @@
 
-angular.module('chezCecEtFlo', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('teal')
-            .accentPalette('light-green')
-            .warnPalette('amber');
-    })
-    .controller('AppCtrl', function($scope) {
-        $scope.title = 'test title';
-    });
+var chezCecEtFlo = angular.module('chezCecEtFlo', ['ngMaterial', 'ngRoute']);
+
+// CONFIG THEME
+chezCecEtFlo.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('light-green')
+        .warnPalette('amber')
+        .backgroundPalette('grey');
+})
+// CONFIG ROUTES
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+        .when('/todo', {
+            templateUrl: 'partials/todo.html',
+            controller: 'TodoCtrl'
+        })
+        .when('/courses', {
+            templateUrl: 'partials/courses.html',
+            controller: 'CoursesCtrl'
+        })
+        .when('/chats', {
+            templateUrl: 'partials/chats.html',
+            controller: 'ChatsCtrl'
+        })
+        .otherwise({
+            redirectTo: '/todo'
+        });
+    }
+]);
--- www/js/app.js
+++ www/js/app.js
@@ -1,42 +1,32 @@
 
-angular.module('chezCecEtFlo', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('teal')
-            .accentPalette('light-green')
-            .warnPalette('amber')
-            .backgroundPalette('grey');
-    })
-    .controller('AppCtrl', function($scope, $mdSidenav, $log, $timeout) {
-        $scope.toggleLeft = buildDelayedToggler('left');
+var chezCecEtFlo = angular.module('chezCecEtFlo', ['ngMaterial', 'ngRoute']);
 
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                    $log.debug("toggle " + navID + " is done");
-                });
-            }, 200);
-        }
-        
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = $scope,
-                args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
-    }).controller('LeftCtrl', function ($scope, $mdSidenav, $log) {
-        $scope.close = function () {
-            $mdSidenav('left').close()
-            .then(function () {
-                $log.debug("close LEFT is done");
-            });
-        };
-    });
+// CONFIG THEME
+chezCecEtFlo.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('light-green')
+        .warnPalette('amber')
+        .backgroundPalette('grey');
+})
+// CONFIG ROUTES
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+        .when('/todo', {
+            templateUrl: 'partials/todo.html',
+            controller: 'TodoCtrl'
+        })
+        .when('/courses', {
+            templateUrl: 'partials/courses.html',
+            controller: 'CoursesCtrl'
+        })
+        .when('/chats', {
+            templateUrl: 'partials/chats.html',
+            controller: 'ChatsCtrl'
+        })
+        .otherwise({
+            redirectTo: '/todo'
+        });
+    }
+]);
